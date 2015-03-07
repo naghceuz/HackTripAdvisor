90b5436c9af69385799827fb9a878754b14291d3
D-shaw <xy19910517@hotmail.com>
2015-03-07 05:55:32 -0500

    ig for loop bug solved

--- db.js
+++ db.js
@@ -21,8 +21,8 @@ var AttractionSchema = new mongoose.Schema({
 
 // A model is like a class with which we construct documents
 var Attractions = mongoose.model('Attractions', AttractionSchema);
-// Call Instagram API
-ig = require('instagram-node').instagram();
+// // Call Instagram API
+var ig = require('instagram-node').instagram();
 
 ig.use({ access_token: '559886220.51c66ef.1503ba29c29748d6930e9e49a7043b2d' });
 ig.use({ client_id: '51c66ef6388449f1a5263daa554a373f',
@@ -30,96 +30,90 @@ ig.use({ client_id: '51c66ef6388449f1a5263daa554a373f',
 
 
 
-require('./tripAdvisor');
+// require('./tripAdvisor');
 
-var attractionTag = ['fenwaypark','mfa','IsabellaStewartGardner','IsabellaStewartGardner',
-              'NewburyStreet','tdgra'];
+var attractionTag = ['fenwaypark','mfa','IsabellaStewartGardner',
+              'NewburyStreet','tdgarden'];
+
+
 
 
 for (i = 0; i < attractionTag.length; i++) {
-  ///* OPTIONS: { [min_tag_id], [max_tag_id] }; */
-  mongooseWrite(i);
+  instagram(i);
 }
 
 
 
-ig.tag_media_recent("HackTripAdvisor" , {max_tag_id:1}, function(err, medias, pagination, remaining, limit) {
-    var theData = medias;
-    var theDatelength = theData.length
+// ig.tag_media_recent("HackTripAdvisor" , {max_tag_id:1}, function(err, medias, pagination, remaining, limit) {
+
+//     var mediasLength = medias.length;
     
-    console.log(theData)
-    console.log("Data length is " + theDatelength)
-    console.log("userid " + theData[0].user.id)
-    console.log("username " + theData[0].user.username)
-    console.log("pic_id: " + theData[0].id)
-    console.log("userid in data1 " + theData[1].user.id)
-     // pic_link:data[0].link,
-     // img_link:data[0].images.standard_resolution.url,
-     // likes:data[0].likes.count,
-     // tagName:data[0].tags[0]
+//     console.log("Data length is " + mediasLength);
+//     console.log("userid = " + medias[0].user.id);
+//     console.log("username = " + medias[0].user.username);
+//     console.log("pic_id = " + medias[0].id);
+//     console.log("pic_link = " + medias[0].link);
+//     console.log("img_link = " + medias[0].images.standard_resolution.url);
+//     console.log("likes = " + medias[0].likes.count);
+//     console.log("tagName = " + medias[0].tags[0]);
 
-});
+// });
 
 
-
-
-function mongooseWrite(i) {
-
-  //attractionTag[i]  = "fenwaypark"
-    ig.tag_media_recent(attractionTag[i] , {max_tag_id:1}, function(err, medias, pagination, remaining, limit) {
-    var theData = medias;
-
-  // Model.findOneAndUpdate([conditions], [update], [options], [callback])
-  Attractions.findOneAndUpdate(
-  // [conditions]
-  {
-     // userid:theData[i].user.id
-     // pic_id:data[0].id
-  },
-  // [update]
-  {
-      userid:theData[i].user.id,
-      username:theData[i].user.username,
-      pic_id: theData[i].id,
-      pic_link:theData[i].link,
-      img_link:theData[i].images.standard_resolution.url,
-      likes:theData[i].likes.count
-  }, 
-  // [callback] if document exists, callback--'found' will be null
-  function (err, found) {
-    // console.log("i = " + i);
-    
-    if(err) {
-      return console.log("MongoDB Error: " + err);
-    }
-    else if(!found) {   // 'found' is null, then create new
-      console.log("Not Exist");
-      //console.log('i = ' + i); 
-
-      // Model.create(doc(s), [callback])
-      Attractions.create (
-      {
-       
-      userid:theData[i].user.id,
-      username:theData[i].user.username,
-      pic_id: theData[i].id,
-      pic_link:theData[i].link,
-      img_link:theData[i].images.standard_resolution.url,
-      likes:theData[i].likes.count
-
-      }, 
-      // [callback]
-      function (err, createItem) {
-        if(err) {
-                return console.log("MongoDB Error: " + err);
-        }
-      });
-    }
-    else {
-      console.log("Exist");
-    }
+function instagram(i) {
+  ig.tag_media_recent(attractionTag[i] , {max_tag_id:1}, function(err, medias, pagination, remaining, limit) {
+    mongooseWrite(medias);
+    // console.log("***\n***\n***\n***\n***\n" + attractionTag[i]);
+    // console.log(medias[0].user.id);
   });
-});
+}
+
+
+function mongooseWrite(medias) {
+
+    // Model.findOneAndUpdate([conditions], [update], [options], [callback])
+    Attractions.findOneAndUpdate(
+    {  // [conditions]
+      userid:medias[0].user.id,
+      // pic_id:medias[0].id,
+    },
+    {  // [update]
+      userid:medias[0].user.id,
+      username:medias[0].user.username,
+      // pic_id: medias[0].id,
+      pic_link:medias[0].link,
+      img_link:medias[0].images.standard_resolution.url,
+      likes:medias[0].likes.count,
+      tagName:medias[0].tags[0],
+    }, // [callback] if document exists, callback--'found' will be null
+      function (err, found) {
+        // console.log("i = " + i);
+        if(err) {
+          return console.log("MongoDB Error: " + err);
+        }
+        else if(!found) {   // 'found' is null, then create new
+          console.log("Not Exist");
+          //console.log('i = ' + i); 
+          Attractions.create (  // Model.create(doc(s), [callback])
+          {
+            userid:medias[0].user.id,
+            username:medias[0].user.username,
+            // pic_id: medias[0].id,
+            pic_link:medias[0].link,
+            img_link:medias[0].images.standard_resolution.url,
+            likes:medias[0].likes.count,
+            tagName:medias[0].tags[0],
+          }, // [callback]
+            function (err, createItem) {
+              if(err) {
+                return console.log("MongoDB Error: " + err);
+              }
+          });
+        }
+        else {
+          console.log("Exist");
+        }
+    });
 }
 
 
--- to-do.md
+++
@@ -1,6 +0,0 @@
-- call TripAdvisor API  
-   http://api.tripadvisor.com/api/partner/2.0/location/60745/attractions?key=HackTripAdvisor-ade29ff43aed
-
-- figure out instgram tag API usage. get 6 Schema elements
-
-- figure out how to write in mongoose database
